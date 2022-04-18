d95d7b764878395bb8397d1563c6ea30066e6bc7
giodefa96 <giodefa96@gmail.com>
2022-04-18 11:52:26 +0200

    new branch

--- bozza/function.py
+++ bozza/function.py
@@ -4,7 +4,6 @@ import sys
 
 class SearchingModelPath:
 
-
     def __init__(self, searching_model_path=None):
         self.searching_model_path = searching_model_path
 
@@ -34,3 +33,32 @@ def searching_model_path(name: str):
         return path_model
     except SystemExit:
         print('Insert string and valid model name')
+
+
+def plot_part_of_map(ens,a,b,c,d):
+    """Plot a part of a map"""
+
+    d1_nat = ens.sel(lat=slice(a,b), lon=slice(c,d))
+    psl_nat=d1_nat.ens_mean
+    fig = plt.figure(figsize=(30,24))  # x,y(inches)
+
+    ax = plt.axes(projection=ccrs.PlateCarree())
+    ax.set_title(''.join(['ciao','\n']))
+    ax.set_global()
+    mm = ax.contourf(psl_nat.lon, psl_nat.lat, psl_nat.mean(axis=0), transform=ccrs.PlateCarree(), cmap='jet')
+
+    ax.coastlines()
+    ax.gridlines(draw_labels=True)
+
+    # add colorbar
+    cbar_ax = fig.add_axes([0.28, 0.10, 0.46, 0.05])
+    cbar = fig.colorbar(mm, cax=cbar_ax, extend='both', orientation='horizontal')
+    # [left, bottom, width, height]
+    cbar.set_label('Temperature')
+    cbar.ax.tick_params(labelsize=8)
+
+    plt.show()
+    plt.close()
+
+    return
+
